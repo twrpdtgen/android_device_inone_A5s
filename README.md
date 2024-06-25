bdd8d73e7b3ce45fc1c31f17113ff09bcdaa9ff5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-06-25 13:32:52 +0200

    A5s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: inone Model: Lenovo L18081 Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Lenovo Lenovo L18081 (A5s)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_A5s.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from A5s device
+$(call inherit-product, device/inone/A5s/device.mk)
+
+PRODUCT_DEVICE := A5s
+PRODUCT_NAME := omni_A5s
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo L18081
+PRODUCT_MANUFACTURER := inone
+
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_lava6761_l05-user 9 PPR1.180610.011 1546088675 release-keys"
+
+BUILD_FINGERPRINT := Lenovo/L18081/A5s:9/PPR1.180610.011/1546088675:user/release-keys
