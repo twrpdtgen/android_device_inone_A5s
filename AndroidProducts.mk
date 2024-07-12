f82fefc4ba64ff7b1a9f0a54ea243ed474d3f5ce
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-07-12 15:22:15 +0200

    A5s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: inone Model: Lenovo L18081 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_A5s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_A5s-user \
+    omni_A5s-userdebug \
+    omni_A5s-eng
